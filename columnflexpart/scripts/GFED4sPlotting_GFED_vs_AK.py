b387e0e1a53f5504490519909678628d35457e28
Ines Dillerup <ines@dillerup.net>
2023-03-23 17:54:43 +0100

    Plot with grey in background ak

--- columnflexpart/scripts/GFED4sPlotting_GFED_vs_AK.py
+++ columnflexpart/scripts/GFED4sPlotting_GFED_vs_AK.py
@@ -18,12 +18,15 @@ from columnflexpart.classes.inversion import InversionBioclass
 from columnflexpart.utils.utils import optimal_lambda
 from matplotlib.colors import LogNorm 
 from matplotlib.colors import LinearSegmentedColormap
+#from matplotlib.patches import Rectangle
+from matplotlib.lines import Line2D
 
 
 
 
 def plot_averaging_kernel(Inversion, alpha, class_num, week_num,savepath, plot_spatially, weekly = False):
     # get ak 
+    print('Calculating Averaging Kernel')
     flux_mean, flux_err = Inversion.get_flux()
     err = Inversion.get_land_ocean_error(1/10)
     predictions = Inversion.fit(alpha = alpha, xerr = err) 
@@ -48,7 +51,8 @@ def plot_averaging_kernel(Inversion, alpha, class_num, week_num,savepath, plot_s
         #ak_final = ak_final(np.where(ak_final>1e-3)[0])
         #print(ak_final)
         ak_xr = xr.DataArray(data = ak_final.diagonal()[1:], dims = ['bioclass'], coords=dict(bioclass= list(np.arange(1,class_num))))#, week = [1,48,49,50,51,52])))
-        print(ak_xr)
+        #print(ak_xr)
+        '''
         selection = np.where(ak_xr>1e-3)[0]
         #print(np.where(ak_xr>1e-3)[0])
         ds0 = ak_xr.where(ak_xr.bioclass==selection[0], drop = True).rename('ak')
@@ -59,11 +63,13 @@ def plot_averaging_kernel(Inversion, alpha, class_num, week_num,savepath, plot_s
             ds0 = xr.merge([ds0,dsi])
         ak_xr = ds0
         #print(ak_xr)
-        
+        '''
         #print(len(ak_xr))
         #print(len(ak_xr.ak))
         #print(ak_xr.ak)       
-        ak_spatial = Inversion.map_on_grid_without_time_coord(ak_xr.ak, len(ak_xr.ak))
+        ak_spatial = Inversion.map_on_grid_without_time_coord(ak_xr, len(ak_xr))#ak_xr.ak, len(ak_xr.ak)
+        print(ak_spatial)
+        print('Done')
         return ak_spatial
         print(ak_spatial)
         ax = plt.axes(projection=ccrs.PlateCarree())  
@@ -93,7 +99,7 @@ def add_iso_line(ax, x, y, z, value, **kwargs):
     v = np.diff(z > value, axis=1) #1
     print(v)
     h = np.diff(z > value, axis=0)#0
-    l = np.argwhere(v.T) #v.T
+    l = np.argwhere(v) #v.T
     print(l)
     print(x.shape)
     print(y.shape)
@@ -102,7 +108,7 @@ def add_iso_line(ax, x, y, z, value, **kwargs):
     print(y[l[:, 1]])
     vlines = np.array(list(zip(np.stack((x[l[:, 0] + 1], y[l[:, 1]])).T, #T
                                 np.stack((x[l[:, 0] + 1], y[l[:, 1] + 1])).T))) #T
-    l = np.argwhere(h.T) #h.T
+    l = np.argwhere(h) #h.T
     hlines = np.array(list(zip(np.stack((x[l[:, 0]], y[l[:, 1] + 1])).T,#T
                                 np.stack((x[l[:, 0] + 1], y[l[:, 1] + 1])).T))) #T
     lines = np.vstack((vlines, hlines)) #vlines, hlines
@@ -111,27 +117,47 @@ def add_iso_line(ax, x, y, z, value, **kwargs):
 
 
 def plotFireMonthlyMeanSpatially(savepath, syear, smonth, gdf, Inversion, savefig=True):
+    print('Processing GFED')
     sdf = gdf[(gdf['Year']==syear)&(gdf['Month']==smonth)].reset_index()
 
     #sdf = sdf[(sdf['Longround']>166)]
     #sdf1 = sdf[( sdf['Lat']<154)]
     sdf = sdf.set_index(['Long', 'Lat'])   
-    print(sdf)
+    #print(sdf)
     xsdf = xr.Dataset.from_dataframe(sdf)
     foo = xr.DataArray(xsdf['total_emission'][:]*12/44)#in TgC
 
     foo1 = foo[(foo['Long']>166)]
     foo2 = foo[( foo['Long']<154)]
-    #foo2 = foo2[(foo2['emission']>0)]
     print(foo2)
+    foo2 = foo2.where(foo2>0.0005)
+    #foo2 = foo2[(foo2['emission']>0)]
+    #print(foo2)
 
     ak_to_plot = plot_averaging_kernel(Inversion, 0.0006228666734243309, 699, 6,savepath, True)
     
     #ak_to_plot = ak_to_plot>1e-3
     #print(ak_to_plot.where(ak_to_plot>1e-4))
-    ak_equal = ak_to_plot.where(ak_to_plot>0, 1, 0)#*1
-    print(ak_equal)
+   
+    ak_equal =  xr.where((ak_to_plot>=5e-2), 5e-2,  ak_to_plot)
+    print('Step 1')
+    print(ak_equal.min())
+    print(ak_equal.max())
+    ak_equal = xr.where((ak_equal<5e-2)&(ak_equal>=1e-2), 1e-2, ak_equal)
+    print('Step 2')
+    print(ak_equal.min())
+    print(ak_equal.max())
+    ak_equal = xr.where((ak_equal<1e-2)&(ak_equal>=1e-3), 1e-3,ak_equal)
+    print('Step 3')
+    print(ak_equal.min())
+    print(ak_equal.max())
+    ak_equal = ak_equal.where(ak_equal>=1e-3)#*1
+    print('Step 4')
+    print(ak_equal.min())
+    print(ak_equal.max())
     
+
+
 
     plt.figure(figsize=(14, 10))
     plt.rcParams.update({'font.size': 13})    
@@ -144,24 +170,43 @@ def plotFireMonthlyMeanSpatially(savepath, syear, smonth, gdf, Inversion, savefi
     gl.ylines = False
     
     cmap0 = LinearSegmentedColormap.from_list('', ['white','orange','orangered', 'firebrick', 'darkred'])#'white',
+    cmap_ak = LinearSegmentedColormap.from_list('', ['white','lightgrey','silver','darkgrey',  'dimgrey'])#, 'black'])#'white',#'gray',
     #colors = ["darkorange", "gold", "lawngreen", "lightseagreen"]
     #cmap1 = LinearSegmentedColormap.from_list("", colors)
 
-    add_iso_line(ax = ax, x = ak_equal.longitude.values-0.5, y = ak_equal.latitude.values-0.5,
-                  z = ak_equal.values, value = 0.5, color="black", linewidth=0.2)
+    #add_iso_line(ax = ax, x = ak_equal.longitude.values-0.5, y = ak_equal.latitude.values-0.5,
+    #              z = ak_equal.values, value = 0.5, color="black", linewidth=0.2)
     #ak_equal.plot.contour(x='longitude', y = 'latitude', ax = ax, levels = 1, colors = 'k')# cmap = 'Greys',norm = LogNorm(vmin = 1e-3),cbar_kwargs=dict(orientation='vertical',
                    #   shrink=0.75, label='Averaging kernel') )
-    #ak_equal.plot(norm = LogNorm(vmin = 1e-3),x='longitude', y = 'latitude', ax = ax, cmap = 'Greys', add_colorbar=False)
+   
+    ak_equal.plot(norm = LogNorm(vmin = 3e-4), x='longitude', y = 'latitude', ax = ax, alpha = 0.9,  cmap = cmap_ak, add_colorbar = False)#alpha = 0.6, #'cmap_akGreys', cbar_kwargs=dict(orientation='vertical',
+               #       shrink=0.75, label='Averaging Kernel', ticks = [1e-4,1e-3, 1e-2, 5e-2]))
     print('ak_plotted')
-    
-    foo2.plot.pcolormesh(x='Long', y = 'Lat',extend = 'max', cmap = cmap0, vmax = 0.2,ax = ax, cbar_kwargs=dict(orientation='vertical',
-                      shrink=0.75, label='CO$_2$ Emissions [TgC/month]', ticks = np.arange(0,0.3,0.1)))#ax= ax,  transform = ccrs.PlateCarree() cmap = 'OrRd',
+
+    foo2.plot.pcolormesh(x='Long', y = 'Lat',extend = 'max', cmap = cmap0, vmax = 0.2,vmin = 0, ax = ax,  cbar_kwargs=dict(orientation='vertical',
+                      shrink=0.9, label='CO$_2$ Emissions [TgC/month]', ticks = np.arange(0,0.3,0.1)))#ax= ax,  transform = ccrs.PlateCarree() cmap = 'OrRd',
     print('GFED plotted')
     #foo2.plot.pcolormesh(x='Longround', y = 'Lat',cmap = cmap0, vmax = 0.2,ax = ax, add_colorbar = False)
-    
+ 
+
+    #plt.plot([], [], ' ',color = 'lightgrey',  label=r">= 1$\cdot$10$^{-3}$")
+    #plt.plot([], [], ' ',color = 'grey',  label=r">= 1$\cdot$10$^{-2}$")
+    #plt.plot([], [], ' ',color = 'darkgrey', label=r">= 5$\cdot$10$^{-2}$")
+    legend_elements = [Line2D([0], [0], color='grey', lw=5, label=r"$\geq 5\cdot 10^{-2}$"),
+                       Line2D([0], [0], color='darkgrey', lw=5, label=r"$< 5\cdot 10^{-2} & \geq 10^{-2}$"),
+                       Line2D([0], [0], color='lightgrey', lw=5, label=r"$< 10^{-2} & \geq 10^{-3}$")]
+                       
+    plt.legend(handles = legend_elements, title = 'Averaging kernel', loc = 'lower left')
+    #bar_10_100 = ax.bar(np.arange(0,10), np.arange(30,40), bottom=np.arange(1,11), color="darkgrey")
+    #bar_0_10 = ax.bar(np.arange(0,10), np.arange(1,11), color="grey")
+    #bar_10_100 = ax.bar(np.arange(0,10), np.arange(30,40), bottom=np.arange(1,11), color="lightgrey")
+    # create blank rectangle
+    #extra = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0)
+    #ax.legend([extra, bar_0_10, bar_10_100], ("Averaging kernel", r">= 5$\cdot$10$^{-2}$",r">= 1$\cdot$10$^{-2}$", r">= 1$\cdot$10$^{-3}$"))
+
     ax.coastlines()
     plt.title('GFED CO$_2$ Emissions '+str(smonth).zfill(2)+'/'+str(syear).zfill(2), size =15)
-    plt.savefig(savepath+"GFED_AK_CO2_AU_contour_iso_line_ak_1e-3.png")
+    plt.savefig(savepath+"GFED_AK_CO2_AU_greys_no_ocean_ak_1e-3.png")
     #plt.title('Fire CO$_2$ Emissions '+str(smonth).zfill(2)+'/'+str(syear).zfill(2), size =15)
     #if savefig == True: 
     #    plt.savefig(savepath+"GFED_CO2_AK_AU_{}_{}_smallerFires.png".format(syear,smonth),facecolor='w',edgecolor='none')
