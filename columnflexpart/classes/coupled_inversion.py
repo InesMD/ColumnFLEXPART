Corrected rho prior: variance matrix, not std

columnflexpart/classes/coupled_inversion.py
@@ -736,17 +736,17 @@ class CoupledInversion(InversionBioclass):# oder von InversionBioClass?
 
         fireCO_diag = np.ones(len_diag_flattened) * COfire_error
         fireCO_diag[0] = fireCO_diag[0]/10000000 
-        fireCO = np.diag(fireCO_diag)
+        fireCO = np.diag(fireCO_diag**2)
         #print(fireCO)
         fireCO2_diag = np.ones(len_diag_flattened) * CO2fire_error
         fireCO2_diag[0] = fireCO2_diag[0]/10000000 
-        fireCO2 = np.diag(fireCO2_diag)
+        fireCO2 = np.diag(fireCO2_diag**2)
 
-        off_diag_fire = np.sqrt(fireCO2_diag*fireCO_diag)
+        off_diag_fire = fireCO2_diag*fireCO_diag
         off_diag_fire_block = np.diag(off_diag_fire)
 
         bio_diag =  np.ones(len_diag_flattened)/10000000 
-        bio = np.diag(bio_diag)
+        bio = np.diag(bio_diag**2)
 
         zero_block = np.zeros_like(bio)
 
@@ -891,7 +891,7 @@ class CoupledInversion(InversionBioclass):# oder von InversionBioClass?
         """Uses bayesian inversion to estiamte emissions.
 
         Args:
-            yerr (Optional[list], optional): Can be used instead of loaded errorof measurement. Defaults to None.
+            yerr (Optional[list], optional): Can be used instead of loaded error of measurement. Defaults to None.
             xerr (Optional[list], optional): Can be used instead of error of the mean of the fluxes. Defaults to None.
             with_prior (Optional[bool]): Wether to use a prior or not. True strongly recommended. Defaults to True.
             alpha (Optional[float]): Value to weigth xerr against yerr. Is used as in l curve calculation. Defaults to 1.
@@ -912,9 +912,9 @@ class CoupledInversion(InversionBioclass):# oder von InversionBioClass?
             data = np.sqrt(np.diag(self.get_posterior_covariance())),
             dims = ["new"],
             coords = dict(new=self.coords)
-        )
+        )# is std deviation 
         #self.prediction_errs = self.prediction_errs_flat.unstack("new")
-        return self.predictions
+        return #self.predictions
     
     def map_on_gridded_grid(self, xarr: xr.DataArray) -> xr.DataArray:#[self.time_coord]
         mapped_xarr = xr.DataArray(

columnflexpart/scripts/do_coupled_inversion.py
@@ -4,10 +4,11 @@ import datetime
 import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
+import pandas as pd
 import cartopy.crs as ccrs
 from columnflexpart.scripts.plotting_routine_coupled_split_inversion import plot_spatial_flux_results_or_diff_to_prior, plot_averaging_kernel,plot_input_mask,plot_weekly_concentrations, plot_single_concentrations
 from columnflexpart.scripts.plotting_routine_coupled_split_inversion import split_eco_flux, split_predictions,  plot_prior_spatially, plot_single_total_concentrations, calculate_and_plot_averaging_kernel, plot_l_curve, split_gridded_flux
-from columnflexpart.scripts.plotting_routine_coupled_split_inversion import plot_emission_ratio
+from columnflexpart.scripts.plotting_routine_coupled_split_inversion import plot_emission_ratio, split_output, plot_spatial_result
 
 
 def do_everything(savepath, Inversion, molecule_name, mask_datapath_and_name, week_min, week_max, week_num):
@@ -18,38 +19,43 @@ def do_everything(savepath, Inversion, molecule_name, mask_datapath_and_name, we
     #l2 =5e-1
     #l2 = 2e-2
     #l3 = 1e-2
-    l1 = 3e-2#3.3e-3
-    #2 = 3.2e-3
+    l1 = 2.4e-2#3.3e-3
+    l2 = 2.3e-2
+    l3 = 2.2e-2
+    l4 = 2.1e-2
     #l3 = 5e-3
     #l3 = 1
     #l4 = 1e-4
     #l5 = 5e-4
     #l6 = 5e-5
-    for l in [l1]:#,l2]:#,l3,l4,l5,l6]:#, l2]:#, l3]:#1, l2]:#,l2,l3]:
+    for l in [l1]:#,l2,l3,l4]:#,l2]:#,l3,l4,l5,l6]:#, l2]:#, l3]:#1, l2]:#,l2,l3]:
         print('fitting')
-        predictions = Inversion.fit(alpha = l)#, xerr = err) 
+        Inversion.fit(alpha = l)#, xerr = err) 
+
+        # save predictions
+        pred = pd.DataFrame(data =Inversion.predictions_flat.values, columns = ['predictions'])
+        pred.insert(loc = 1, column ='posterior_err_std', value = Inversion.prediction_errs_flat.values)
+        pred.insert(loc = 1, column ='prior_err_cov', value = np.diagonal(Inversion.flux_errs_flat.values))
+        pred.insert(loc = 1, column ='prior_err_cov_with_lambda', value = np.diagonal(Inversion.flux_errs_flat.values)/l)
+        pred.to_pickle(savepath + str(l)+'_predictions.pkl')
+        
         Inversion.predictions_flat = Inversion.predictions_flat.rename(dict(new = 'bioclass'))
         #plot_l_curve(Inversion,err,molecule_name,savepath, l)
-        predictionsCO2_fire, predictionsCO2_bio, predictionsCO_fire, predictionsCO_bio = split_predictions(Inversion)
-        fluxCO2_fire, fluxCO2_bio, fluxCO_fire, fluxCO_bio = split_eco_flux(Inversion)
+        predictions_list = split_output(Inversion.predictions_flat, 'bioclass')
+        fluxCO2_fire, fluxCO2_bio, fluxCO_fire, fluxCO_bio = split_eco_flux(Inversion) ### change to use split_output too 
+        post_std_list = split_output(Inversion.prediction_errs_flat, 'new')
+        prior_std_list = split_output(np.sqrt(np.diagonal(Inversion.flux_errs_flat.values)/l), 'new')
 
         flux_list = [fluxCO2_fire, fluxCO2_bio, fluxCO_fire, fluxCO_bio]
-        name_list = ['CO2_fire_small_prior', 'CO2_ant_bio_small_prior_', 'CO_fire_small_prior_', 'CO_ant_bio_small_prior']  
+        name_list = ['CO2_fire', 'CO2_ant_bio', 'CO_fire', 'CO_ant_bio']  
 
         fluxCO2_fire_grid, fluxCO2_bio_grid, fluxCO_fire_grid, fluxCO_bio_grid = split_gridded_flux(Inversion)
 
         flux_list_grid = [fluxCO2_fire_grid, fluxCO2_bio_grid, fluxCO_fire_grid, fluxCO_bio_grid]
-        name_list_grid = ['CO2_fire_small_prior_grid', 'CO2_ant_bio_small_prior_grid', 'CO_fire_small_prior_grid', 'CO_ant_bio_small_prior_grid']  
-
-        print('CO2_fire')
-        print(predictionsCO2_fire)
-        print('CO_fire')
-        print(predictionsCO_fire)
-        print('ratio flux')
-        print(fluxCO2_fire/fluxCO_fire*44/28*predictionsCO2_fire/predictionsCO_fire)
-        
-
-        for idx,predictions in enumerate([predictionsCO2_fire,predictionsCO2_bio,predictionsCO_fire,predictionsCO_bio]):
+        name_list_grid = ['CO2_fire_grid', 'CO2_ant_bio_grid', 'CO_fire_grid', 'CO_ant_bio_grid']  
+      
+ 
+        for idx,predictions in enumerate(predictions_list):
             print('Plotting prior spatially')
             plot_prior_spatially(Inversion, flux_list[idx],name_list[idx], idx, savepath)
             #plot_prior_spatially(Inversion, flux_list_grid[idx],name_list_grid[idx], idx,savepath, False)
@@ -60,11 +66,18 @@ def do_everything(savepath, Inversion, molecule_name, mask_datapath_and_name, we
             print('Plotting spatial results')
             plot_spatial_flux_results_or_diff_to_prior(savepath, Inversion, predictions, flux_list[idx], name_list[idx],idx, l, diff =True)
 
+            print('Plot posterior std deviation')
+            plot_spatial_result(Inversion.map_on_grid(post_std_list[idx]), savepath, str(l)+'post_std_'+name_list[idx]+'.png', 'plasma', vmax =None, vmin =0, 
+                                cbar_kwargs = {'shrink':  0.835, 'label' : r'posterior standard deviation'}, norm = None)
+
+            #(prior variance(lambda considered) - posterior variance)/prior variance = (prior variance/lambda - post variance)/prior variance
+            print('Plot error variance reduction/Uncertainty reduction')
+
             print('Plotting averaging kernels') # not yet working?! CHeck results!!!!!!!!!!!!!!!! 
             #plot_averaging_kernel(Inversion, l, class_num, week_num,savepath, plot_spatially=False)# class_num
             #plot_averaging_kernel(Inversion, l, class_num, week_num,savepath, plot_spatially=True,weekly = True)
             #plot_averaging_kernel(Inversion, l, class_num, week_num,savepath, plot_spatially=True)
-        plot_emission_ratio(savepath, Inversion, predictionsCO2_fire, predictionsCO_fire, l, fluxCO2_fire, fluxCO_fire)
+        plot_emission_ratio(savepath, Inversion, predictions_list[0], predictions_list[2], l, fluxCO2_fire, fluxCO_fire)
         print('Plotting concentrations')
         total_CO2, total_CO = plot_single_concentrations(Inversion, l, savepath)
         #plot_single_concentrations(Inversion, predictionsCO2_fire, predictionsCO2_bio, fluxCO2_fire,fluxCO2_bio, 'CO2',l, savepath)
@@ -75,12 +88,12 @@ def do_everything(savepath, Inversion, molecule_name, mask_datapath_and_name, we
         calculate_and_plot_averaging_kernel(Inversion,savepath,l)
         plot_l_curve(Inversion, molecule_name, savepath, l, err = None)
         
-
+ 
         #plot_single_concentrations(Inversion,  'CO',l, savepath)
         #plot_weekly_concentrations(Inversion,'CO',l, savepath) # ist ziemlich hartgecoded gerade für Dezember!!!!!!!!!!
         #plot_weekly_concentrations(Inversion,'CO2',l, savepath)
 
-        
+
 ######################### adapt stuff from here on ####################################
 
 savepath = '/work/bb1170/RUN/b382105/Flexpart/TCCON/output/one_hour_runs/CO2/Images_coupled_Inversion/everything_splitted_first_correlation_setup_weekly_inversion/CO_like_CO2_prior/Working_Setup/CO2_200_CO_200/CO2_2_CO_6/'

columnflexpart/scripts/plotting_routine_coupled_split_inversion.py
@@ -21,7 +21,24 @@ def split_predictions(Inversion):
     predictionsCO_fire = predictionsCO_fire.assign_coords(bioclass = np.arange(0,len(predictionsCO2_fire['bioclass'])))
     predictionsCO_bio = predictionsCO_bio.assign_coords(bioclass = np.arange(0,len(predictionsCO2_fire['bioclass'])))
 
-    return predictionsCO2_fire, predictionsCO2_bio, predictionsCO_fire, predictionsCO_bio
+    return [predictionsCO2_fire, predictionsCO2_bio, predictionsCO_fire, predictionsCO_bio]
+
+
+def split_output(xarray_to_split, variable_name: str):
+    predictionsCO2_fire = xarray_to_split[xarray_to_split[variable_name]<xarray_to_split[variable_name].shape[0]/4]
+    print(predictionsCO2_fire)
+    predictionsCO2_bio = xarray_to_split[(xarray_to_split[variable_name]<xarray_to_split[variable_name].shape[0]/2)
+                                                &(xarray_to_split[variable_name]>=xarray_to_split[variable_name].shape[0]/4)]
+    predictionsCO_fire = xarray_to_split[(xarray_to_split[variable_name]>=xarray_to_split[variable_name].shape[0]/2)
+                                               &(xarray_to_split[variable_name]<xarray_to_split[variable_name].shape[0]*3/4)]
+    predictionsCO_bio = xarray_to_split[(xarray_to_split[variable_name]>=xarray_to_split[variable_name].shape[0]*3/4)]
+
+    predictionsCO2_bio = predictionsCO2_bio.assign_coords(bioclass = np.arange(0,len(predictionsCO2_fire[variable_name])))
+    predictionsCO_fire = predictionsCO_fire.assign_coords(bioclass = np.arange(0,len(predictionsCO2_fire[variable_name])))
+    predictionsCO_bio = predictionsCO_bio.assign_coords(bioclass = np.arange(0,len(predictionsCO2_fire[variable_name])))
+
+    return [predictionsCO2_fire, predictionsCO2_bio, predictionsCO_fire, predictionsCO_bio]
+
 
 def split_eco_flux(Inversion):
         flux_eco = Inversion.flux_eco[:,Inversion.flux_eco.week == Inversion.week]
@@ -184,11 +201,11 @@ def plot_spatial_flux_results_or_diff_to_prior(savepath, Inversion, predictions,
         spatial_flux = Inversion.map_on_grid(predictions * flux)*factor
 
         if idx == 0: 
-            plot_spatial_result(spatial_flux, savepath, savename, 'seismic',vmax = 600, vmin = -600, cbar_kwargs = {'label' : r'weekly flux [$\mu$gC m$^{-2}$ s$^{-1}$]', 'shrink':  0.835})
+            plot_spatial_result(spatial_flux, savepath, savename, 'seismic',vmax = 250, vmin = -250, cbar_kwargs = {'label' : r'weekly flux [$\mu$gC m$^{-2}$ s$^{-1}$]', 'shrink':  0.835})
         elif idx == 1:
             plot_spatial_result(spatial_flux, savepath, savename, 'seismic', vmax = 10, vmin = -10,cbar_kwargs = {'label' : r'weekly flux [$\mu$gC m$^{-2}$ s$^{-1}$]', 'shrink':  0.835})
         elif idx == 2:
-            plot_spatial_result(spatial_flux, savepath, savename, 'seismic',vmax = 30, vmin = -30, cbar_kwargs = {'label' : r'weekly flux [$\mu$gC m$^{-2}$ s$^{-1}$]', 'shrink':  0.835})
+            plot_spatial_result(spatial_flux, savepath, savename, 'seismic',vmax = 10, vmin = -10, cbar_kwargs = {'label' : r'weekly flux [$\mu$gC m$^{-2}$ s$^{-1}$]', 'shrink':  0.835})
         elif idx == 3:
             plot_spatial_result(spatial_flux, savepath, savename, 'seismic', vmax = 0.1, vmin = -0.1,cbar_kwargs = {'label' : r'weekly flux [$\mu$gC m$^{-2}$ s$^{-1}$]', 'shrink':  0.835})
 